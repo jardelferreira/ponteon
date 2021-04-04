96978eb70e203893afcbe9faef828391317b876b
jardelferreira <jardelferreira.sousa@gmail.com>
2021-04-03 22:24:09 -0300

    make migration networks table

---
+++ database/migrations/2021_04_04_011907_create_networks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNetworksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('networks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',55);
+            $table->string('phone',15)->unique();
+            $table->string('uf',2);
+            $table->string('city',55);
+            $table->unsignedBigInteger('network_id')->nullable();
+            $table->foreign('network_id')->references('id')->on('networks')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('networks');
+    }
+}
