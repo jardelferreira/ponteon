795988b53d8a922324ea07bb2aa74c928d3b0d6d
jardelferreira <jardelferreira.sousa@gmail.com>
2021-04-03 21:51:11 -0300

    Creating Network Models and Controller

---
+++ app/Models/API/Network.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\API;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Network extends Model
+{
+    use HasFactory, Notifiable;
+}
